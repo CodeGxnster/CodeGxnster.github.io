3100cf3e47a6cca1d44ad1765d2b32e6ffb537b8
Frankelly Cordero <frankellycordero88@gmail.com>
2025-08-29 22:01:36 -0400

    Portfolio prototipe 0.1

--- main.js
+++ main.js
@@ -1,42 +1,57 @@
+const sword = document.getElementById('sword');
+const swordShadow = document.getElementById('sword-shadow');
+const swordAudio = document.querySelector('audio[src="sword-audio"]');
+const star = document.getElementById('star');
+const starShadow = document.getElementById('star-shadow');
+const arrow = document.getElementById('arrow');
+const arrowShadow = document.getElementById('arrow-shadow');
 
+// Animation for the sword
+function swordAnimation() {
 
-// canvas
-/* const canvas = document.getElementById('canvas1');
+   sword.style.transform = 'translateY(-40px)';
+   swordShadow.style.transform = 'translateY(-40px)';
+    swordShadow.style.opacity = 100;
+    swordAudio.volume = 0.5; // Set volume to 50%
+    swordAudio.playbackRate = 1.5; // Speed up the audio playback
+    swordAudio.play();
+   swordAudio.currentTime = 0; // Reset audio to start
+   sword.style.cursor = 'pointer';
 
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 550;
-const CANVAS_HEIGHT = canvas.height = 530;
-const imageWidth = 2200;
+    
 
-
-const image1 = new Image();
-image1.src = 'image-prototype.png'
-
-let spriteWidth = 550;
-let spriteHeight = 530;
-let x = 0;
-let frame = 0;
-function carrusel() {
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(image1,x * spriteWidth,0,spriteWidth,spriteHeight,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    frame++;
-    if (frame % 30 === 0) {
-     x++;
-   } else if (x > 3) {
-    x = 0;
-   }
-  
-    requestAnimationFrame(carrusel);
 }
-carrusel();
-console.log(image1)
-// canvas */
-
-
-
-
-
-
+function swordAnimationOut() {
+    sword.style.transform = 'translateY(0px)';
+   swordShadow.style.transform = 'translateY(0px)';
+   swordShadow.style.opacity = 0;
+}
+// Animation for the star
+function starAnimation() {
+    star.style.transform = 'translateY(-40px)';
+    starShadow.style.transform = 'translateY(-40px)';
+    starShadow.style.opacity = 100;
+    star.style.cursor = 'pointer';
+}
+function starAnimationOut() {
+    star.style.transform = 'translateY(0px)';
+    starShadow.style.transform = 'translateY(0px)';
+    starShadow.style.opacity = 0;
+}
+// Animation for the arrow
+function arrowAnimation() {
+    // arrow.style.transform = 'translateY(-40px)';
+    // arrowShadow.style.transform = 'translateY(-40px)';
+    arrow.style.transform = 'rotate(20deg)';
+    arrowShadow.style.transform = 'rotate(20deg)';
+    arrowShadow.style.opacity = 100;
+    arrow.style.cursor = 'pointer';
+}
+function arrowAnimationOut() {
+    arrow.style.transform = 'translateY(0px)';
+    arrowShadow.style.transform = 'translateY(0px)';
+    arrowShadow.style.opacity = 0;
+}
 
 // About seccion
 const tabLinks = document.getElementsByClassName('tab-links');
@@ -53,11 +68,11 @@ xIcon.addEventListener('click', function() {
     menu.style.right = '-200px';
     
 });
-function openTab(tabname) {
-    for(tablink of tabLinks){
-    tablink.classList.remove('active-link');
-}
-    for(tabcontent of tabContent){
+function openTab(event, tabname) {
+    for (let tablink of tabLinks) {
+        tablink.classList.remove('active-link');
+    }
+    for (let tabcontent of tabContent) {
         tabcontent.classList.remove('active-tab');
     }
     event.currentTarget.classList.add('active-link');
@@ -66,5 +81,13 @@ function openTab(tabname) {
 
 
 // About seccion end
+// Events
+let swordMouseOver = sword.addEventListener('mouseover', swordAnimation);
+let swordMouseOut = sword.addEventListener('mouseout', swordAnimationOut);
+let starMouseOver = star.addEventListener('mouseover', starAnimation);
+let starMouseOut = star.addEventListener('mouseout', starAnimationOut);
+let arrowMouseOver = arrow.addEventListener('mouseover', arrowAnimation);
+let arrowMouseOut = arrow.addEventListener('mouseout', arrowAnimationOut);
+
 
 
